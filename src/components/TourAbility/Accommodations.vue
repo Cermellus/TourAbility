66d3f6d7d8a2690946276d4d5fca8b08a5b2c495
Rubens De Giorgio <rubens.degiorgio@axiante.com>
2020-04-26 04:36:00 +0200

    checklist

--- src/components/TourAbility/Accommodations.vue
+++ src/components/TourAbility/Accommodations.vue
@@ -25,7 +25,7 @@
 
 
 
-        <DataView :value="structures" :layout="layout" paginatorPosition="both" :paginator="true" :rows="20"
+        <DataView :value="this.structures" :layout="layout" paginatorPosition="both" :paginator="true" :rows="20"
                   :sortOrder="sortOrder" :sortField="sortField">
             <template #header>
                 <div class="p-grid p-nogutter">
@@ -65,7 +65,7 @@
 
                             </div>
                         </div>
-                        <Button icon="pi pi-plus"></Button>
+                        <Button icon="pi pi-plus" @click="open(slotProps.data)"></Button>
                     </div>
                 </div>
             </template>
@@ -89,11 +89,67 @@
                                     </div>
                                 </div>
 
-                        <Button icon="pi pi-plus"></Button>
+                        <Button icon="pi pi-plus" @click="open(slotProps.data)"></Button>
                     </Panel>
                 </div>
             </template>
         </DataView>
+
+
+
+
+
+				<Dialog :header="'Request for certification '+dialogTitle" :visible.sync="display" :style="{width: '70vw'}" :modal="true">
+                    
+                    <div v-if="showBLev1">
+                        <Button  :style="{width: '100%','margin-bottom':'20px','margin-top':'10px'}" label="Level 1"  icon="pi pi-plus"/>
+                    </div>
+                    <div v-else class="layout-dashboard" >
+                        <div class="p-grid">
+                            <div class="p-col-12 p-md-6 p-lg-4 task-list">
+                                <div class="card card-w-title tasks" >
+                                    <h1>Level</h1>
+                                    <ul>
+                                        <li v-for="cert in CL1.checklist" :key="cert.id">
+                                            <Checkbox name="task" value="report" v-model="tasksCheckbox"/>
+                                            <span>{{cert.title}}</span>
+                                            <span class="task-badge red"></span>
+                                        </li>
+                                       
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--<CheckList v-else name="test1" v-bind:title="post.title" ></CheckList>-->
+
+                    <Button v-if="showBLev2" :style="{width: '100%','margin-bottom':'20px','margin-top':'10px'}" label="Level 2"  icon="pi pi-plus"/>
+                    <div v-else class="layout-dashboard" >
+                        <div class="p-grid">
+                            <div class="p-col-12 p-md-6 p-lg-4 task-list">
+                                <div class="card card-w-title tasks" >
+                                    <h1>Level</h1>
+                                    <ul>
+                                        <li v-for="cert in CL2.checklist" :key="cert.id">
+                                            <Checkbox name="task" value="reports" v-model="tasksCheckbox"/>
+                                            <span>{{cert.title}}</span>
+                                            <span class="task-badge red"></span>
+                                        </li>
+                                       
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <Button v-if="showBLev3" :style="{width: '100%','margin-bottom':'20px','margin-top':'10px'}" label="Level 3"  icon="pi pi-plus"/>
+                   
+					<template #footer>
+						<!--<Button label="Yes" @click="close" icon="pi pi-check"/>-->
+						<Button label="Close" @click="close" icon="pi pi-times" class="p-button-secondary"/>
+					</template>
+				</Dialog>
+
 
 
     </div>
@@ -101,19 +157,37 @@
 
 <script>
     import MockService from '../../service/TourAbility/MockService';
+    //import CheckList from './CheckList.vue';
 
     export default {
         data() {
             return {
-                fc1: false,
+                fc1: true,
                 fc2: true,
                 fc3: true,
-                fc4: false,
-                fc5: false,
-                fc6: false,
+                fc4: true,
+                fc5: true,
+                fc6: true,
+
+                dialogTitle:"",
+                showBLev1:true,
+                showBLev2:true,
+                showBLev3:true,
+                
+                showTask1:false,
+                showTask2:false,
+                showTask3:false,
+
+               tasksCheckbox: [],
+
+                CL1:{},
+                CL2:{},
+                CL3:{},
+
 
                 display: false,
                 structures: null,
+                //visibleStructures:null,
                 cars: null,
                 layout: 'list',
                 sortKey: null,
@@ -132,10 +206,50 @@
         },
         mounted() {
             this.mockService.getCarsLarge().then(data => this.cars = data);
-            this.mockService.getStructures().then(data => this.structures = data);
+            this.mockService.getStructures().then(data => {
+                this.structures = data
+                //this.visibleStructures = data
+            });
         },
         methods: {
+            open(structure) {
+
+                this.dialogTitle=structure.name;
+                
+                let certifications=structure.certifications;
+                
+                if(structure.certifications.level1){
+                   this.showBLev1=false;
+                   this.showTask1=false;
+               }
+               this.CL1=certifications.level1;
+
+            if(structure.certifications.level2){
+                   this.showBLev2=false;
+                   this.showTask2=false;
+               }
+               this.CL2=certifications.level2;
+
+            if(structure.certifications.level3){
+                   this.showBLev3=false;
+                   this.showTask3=false;
+               }
+                this.CL3=certifications.level3;
+
+console.log(this.CL1);
+console.log(this.CL2);
+console.log(this.CL3);
+          
+
+                this.display = true;
+            },
+            close() {
+                this.certifications=false;
+                this.display = false;
+            },
             fcSimulation(fcId){
+                //this.visibleStructures=this.structures
+
                 if(fcId==1){
                     if( this.fc1===true){
                     this.fc2=true;
@@ -160,6 +274,10 @@
                          this.fc1=false;
                      }
                 }
+/*
+            this.visibleStructures.forEach(row => {
+                if(this.fc1)
+            });*/
             },
             onSortChange(event) {
                 const value = event.value.value;
