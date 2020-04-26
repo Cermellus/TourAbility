b3bff964fb1cecc504f2733d26de316c594824ea
Marcello <marcello.mordente-ext@coface.com>
2020-04-26 16:33:17 +0200

    minor topbar fix

--- src/AppTopbar.vue
+++ src/AppTopbar.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="layout-topbar clearfix">
+    <div class="layout-topbar clearfix">
         <div class="layout-menu-container" @click="onMenuClick">
             <div class="layout-menu-wrapper">
                 <div class="menu-scroll-content">
@@ -9,40 +9,40 @@
             </div>
         </div>
 
-		<button class="layout-topbar-logo p-link">
-			<img id="layout-topbar-logo" alt="babylon-layout" src="assets/layout/images/tourability_logo.png"/>
-		</button>
+        <button class="layout-topbar-logo p-link">
+            <img id="layout-topbar-logo" alt="babylon-layout" src="assets/layout/images/tourability_logo.png"/>
+        </button>
 
-		<button class="layout-menu-button p-link" @click="onMenuButtonClick">
-			<i class="pi pi-bars"></i>
-		</button>
+        <button class="layout-menu-button p-link" @click="onMenuButtonClick">
+            <i class="pi pi-bars"></i>
+        </button>
 
-		<button class="p-link signout">
-			<i class="pi pi-sign-out"></i>
-		</button>
-	</div>
+        <button class="p-link signout">
+            <i class="pi pi-sign-out"></i>
+        </button>
+    </div>
 </template>
 
 <script>
     import AppMenu from './AppMenu';
 
-export default {
-	props: {
-		topbarMenuActive: Boolean,
-		activeTopbarItem: String,
-		profileMode: String,
+    export default {
+        props: {
+            topbarMenuActive: Boolean,
+            activeTopbarItem: String,
+            profileMode: String,
             horizontal: Boolean,
 
             menuActive: Boolean,
             model: Array,
             layoutMode: String
         },
-    methods: {
-        onMenuButtonClick(event) {
-            this.$emit('menubutton-click', event);
-        },
-		onTopbarMenuButtonClick(event){
-			this.$emit('topbar-menubutton-click', event);
+        methods: {
+            onMenuButtonClick(event) {
+                this.$emit('menubutton-click', event);
+            },
+            onTopbarMenuButtonClick(event) {
+                this.$emit('topbar-menubutton-click', event);
             },
             onMenuItemClick(event) {
                 this.$emit('menuitem-click', event);
@@ -55,57 +55,69 @@ export default {
             },
             doLogout() {
                 this.$router.replace('/login');
-		}
-    },
-	computed: {
-		topbarItemsClass() {
-			return ['topbar-menu fadeInDown', {
-				'topbar-menu-visible': this.topbarMenuActive
-			}];
-		},
-		displayInlineProfile() {
-			return this.profileMode === 'inline' && this.layoutMode !== 'horizontal'
-		}
+            }
+        },
+        computed: {
+            topbarItemsClass() {
+                return ['topbar-menu fadeInDown', {
+                    'topbar-menu-visible': this.topbarMenuActive
+                }];
+            },
+            displayInlineProfile() {
+                return this.profileMode === 'inline' && this.layoutMode !== 'horizontal'
+            }
         },
         components: {
             'AppMenu': AppMenu,
-	}
+        }
 
-}
+    }
 </script>
 <style lang="scss" scoped>
-	.layout-topbar-logo{
-		width: 25%;
-		padding: 0;
-		img {
-			margin: -2px -2px -2px -60%!important;
-			width: 100%;
-			height: auto!important;
-			min-height: 50px!important;
-			max-height: 80px!important;
-			min-width: 230px!important;
-			max-width: 350px!important;
-			padding-right: 1em;
+    .layout-topbar-logo {
+        width: 25%;
+        padding: 0;
 
-		}
-	}
-	.signout i{
-		position: fixed;
-		top:0.5em;
-		right: 1em;
-		z-index: 999;
-		color: #FDE3d7;
-		font-size: 1.7em;
-	}
+        img {
+            margin: -2px -2px -2px 10px !important;
+            width: 100%;
+            height: auto !important;
+            min-height: 50px !important;
+            max-height: 80px !important;
+            min-width: 230px !important;
+            max-width: 350px !important;
+            padding-right: 1em;
 
-	@media screen and (min-width: 897px) {
-		.layout-topbar-logo{
-			img {
-				margin: -2px!important;}
-		}
-		.signout i{
-			top:1em;
-		}
-	}
+        }
+    }
+
+    .signout i {
+        position: fixed;
+        top: 0.5em;
+        right: .5em;
+        z-index: 999;
+        color: #FDE3d7;
+        font-size: 1.7em;
+    }
+
+    @media screen and (max-width: 897px) {
+        .layout-wrapper {
+            .layout-topbar {
+                text-align: left !important;
+            }
+
+        }
+    }
+
+    @media screen and (min-width: 897px) {
+        .layout-topbar-logo {
+            img {
+                margin: -2px !important;
+            }
+        }
+        .signout i {
+            top: 1em;
+        }
+    }
 
 </style>
